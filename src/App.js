adf3761c79acca0fcd1cc0edd0aa4f2f219dcc3f
Baswal <balapriya1801@gmail.com>
2024-04-26 02:41:15 +0530

    ArrayObj UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //Array of objects static state (Hard-coded Values).
+  const [characters, setCharacters] = useState([
+    { id: 263, name: "Spiderman" },
+    { id: 746, name: "BatMan" },
+    { id: 962, name: "Hulk" },
+    { id: 542, name: "Caption America" },
+    { id: 751, name: "Iron Man" },
+  ]);
+
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    //Filter function is used here to demonstrate the delete function, same as map,
+    //it iterates over the array and based on a specific condition it filter out the items.
+    setCharacters(characters.filter((character) => character.id !== id));
+  }
+
+  function toggle() {
+    setShow(!show);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <button className="toggle" onClick={() => toggle()}>Toggle</button>
+        {/* Map traverse along each item in the array obj one by one */}
+        {show &&
+          characters.map((character) => (
+            //Unique identifier?? So that React at the end of the day react can identify what us what, like generally
+            //it is used for performance and component identity.
+            <li key={character.id}>
+              <span>
+                {character.id} - {character.name}
+              </span>
+              <button
+                className="delete"
+                onClick={() => handleDelete(character.id)}>Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
